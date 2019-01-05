c542b038db43a0e89dc8bb8c8af08f770b9f81be
Ben MacLaurin <hello@benmaclaurin.com>
2019-01-05 20:25:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-exports.box = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 250.854 250.854"> <defs><style>.cls-1{fill:#f5f5f5}.cls-2{fill:none;stroke:#a5a5a5;stroke-dasharray:5}</style></defs> <g id="Group_1" data-name="Group 1" transform="translate(-246 -186)"> <rect id="Rectangle_1" data-name="Rectangle 1" class="cls-1" width="250" height="250" transform="translate(246 186)"/> <line id="Line_1" data-name="Line 1" class="cls-2" y2="250" transform="translate(371.5 186.5)"/> <line id="Line_2" data-name="Line 2" class="cls-2" x2="250" transform="translate(246.5 311.5)"/> <line id="Line_3" data-name="Line 3" class="cls-2" x1="250" y1="250" transform="translate(246.5 186.5)"/> <line id="Line_4" data-name="Line 4" class="cls-2" y1="250" x2="250" transform="translate(246.5 186.5)"/> </g> </svg>';
+exports.box = function(rows) {
+  var box = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 335.854 335.854"> <defs><style>.cls-1{fill:#f2f2f2}.cls-2{fill:none;stroke:#d8d8d8;stroke-dasharray:10}</style></defs> <g id="Group_1" data-name="Group 1" transform="translate(-339 -201)"> <rect id="Rectangle_1" data-name="Rectangle 1" class="cls-1" width="335" height="335" transform="translate(339 201)"/> <line id="Line_2" data-name="Line 2" class="cls-2" x2="335" transform="translate(339.5 368.5)"/> <line id="Line_1" data-name="Line 1" class="cls-2" y2="335" transform="translate(506.5 201.5)"/> <line id="Line_3" data-name="Line 3" class="cls-2" x1="335" y2="335" transform="translate(339.5 201.5)"/> <line id="Line_4" data-name="Line 4" class="cls-2" x2="335" y2="335" transform="translate(339.5 201.5)"/> </g> </svg>';
+  var response = '<table style="width: 90%">';
+  for (i = 0; i < rows; i++) {
+    response += '<tr><td>' + box + '</td></tr>'
+  }
+  response += '</table>';
+};
